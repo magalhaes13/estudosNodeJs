ccc73614ac36d8728ac351726b0378eb27962d01
magalhaes13 <arthurmagalhaes13@hotmail.com>
2022-03-03 00:08:09 -0300

    mudanca

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/menu', function(req,res){
 app.get('/ola/:nome/:cargo/:apelido', function(req,res){
     res.send("<h1>Ola "+req.params.nome+"</h1>" + "<h2> Seu cargo eh: "+req.params.cargo+ "</h2>" + "<h3> Sua cor favorida eh: "+req.params.apelido+ "</h3>");
 });
-//aaaaaaa
+
 app.listen(8081, function(){
     console.log("Servidor rodando");
 });
